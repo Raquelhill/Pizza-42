dcf434fc81fd18b615bc9b38fb1ef24b341d9fbc
Raquel Hill <raquelturing@gmail.com>
2022-01-25 18:15:01 -0700

    Add footer to app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import LoginForm from '../LoginForm/LoginForm';
 import Header from '../Header/Header';
+import Footer from '../Footer/Footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <LoginForm />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+  return (
+    <div>
+      <p className="copyright">©️2022 Pizza 42, LLC. All Rights Reserved.</p>
+    </div>
+  );
+};
+
+export default Footer;
