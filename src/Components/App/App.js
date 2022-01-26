8a2eb443f5218323727b387880c6e31da06c397c
Raquel Hill <raquelturing@gmail.com>
2022-01-25 20:39:36 -0700

    Paste in code from the call an API documentation

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,6 +2,7 @@ import './App.css';
 // import LoginForm from '../LoginForm/LoginForm';
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
+import Profile from '../Profile/Profile';
 import LoginButton from '../LoginButton/LoginButton';
 import LogoutButton from '../LogoutButton/LogoutButton';
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <Header />
+      <Profile />
       {/* <LoginForm /> */}
       <LoginButton />
       <LogoutButton />
