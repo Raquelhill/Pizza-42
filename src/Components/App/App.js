8e62c9c1fff635275f6aa74454b5975e54fa5624
Raquel Hill <raquelturing@gmail.com>
2022-01-25 18:10:58 -0700

    Add header to app component

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import LoginForm from '../LoginForm/LoginForm';
+import Header from '../Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <LoginForm />
     </div>
   );
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Pizza 42</h1>
+    </div>
+  );
+};
+
+export default Header;
