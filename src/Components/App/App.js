6138d12ed5cadfdd65d64efc57ee3dc4eb7f4083
Raquel Hill <raquelturing@gmail.com>
2022-02-06 16:00:41 -0700

    Add config files to main

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Header from '../Header/Header';
 import LandingPage from '../LandingPage/LandingPage';
 import Footer from '../Footer/Footer';
-import OrderCardContainer from '../OrderContainer/OrderCardContainer';
+import OrderCardContainer from '../OrderCardContainer/OrderCardContainer';
 import Profile from '../Profile/Profile';
 import LoginButton from '../LoginButton/LoginButton';
 import LogoutButton from '../LogoutButton/LogoutButton';
---
+++ src/Components/OrderCardContainer/OrderCardContainer.js
@@ -0,0 +1,50 @@
+import './OrderCardContainer.css';
+
+const OrderCardContainer = () => {
+  return (
+    <>
+      <div>
+        <h1>Cheese Pizza</h1>
+        <img
+          src="https://homechef.imgix.net/https%3A%2F%2Fasset.homechef.com%2Fuploads%2Fmeal%2Fplated%2F2996%2Fhomechef_Chicago_Style_Spinach_Deep_Dish_Pizza__8_of_9_.jpg?ixlib=rails-1.1.0&w=1700&auto=format&s=4c6c1797d6bfeca1598067e5eb2f2355"
+          alt="Cheese Pizza"
+        >
+          {' '}
+        </img>
+        <button>Order Now</button>
+      </div>
+      <div>
+        <h1>Pepperoni Pizza</h1>
+        <img
+          src="https://cdn.unos.com/images/menus/deepdish/pepperoni.jpg"
+          alt="Pepperoni Pizza"
+        >
+          {' '}
+        </img>
+        <button>Order Now</button>
+      </div>
+      <div>
+        <h1>Sausage Pizza</h1>
+        <img
+          src="https://cdn.unos.com/images/menus/deepdish/DeepDish_MeatballRicotta_300.jpg"
+          alt="Sausage Pizza"
+        >
+          {' '}
+        </img>
+        <button>Order Now</button>
+      </div>
+      <div>
+        <h1>Vegetarian Pizza</h1>
+        <img
+          src="https://cdn.unos.com/images/menus/deepdish/numero-uno.jpg"
+          alt="Vegetarian Pizza"
+        >
+          {' '}
+        </img>
+        <button>Order Now</button>
+      </div>
+    </>
+  );
+};
+
+export default OrderCardContainer;
